078558f0858be7a058735d9e83406be45bc34fc1
Kaloyan Raev <kaloyan.r@zend.com>
2013-10-13 23:46:06 +0300

    git.bat uses JAVA_HOME

--- org.eclipse.jgit.pgm.win32/git.bat
+++ org.eclipse.jgit.pgm.win32/git.bat
@@ -1,1 +1,3 @@
-java -cp %JGIT_CLASSPATH% org.eclipse.jgit.pgm.Main %*
+@echo off
+if defined JAVA_HOME (set JAVA="%JAVA_HOME%\bin\java") ELSE (set JAVA=java)
+%JAVA% -cp %JGIT_CLASSPATH% org.eclipse.jgit.pgm.Main %*
