1f7a984d02e6ebc35db715c1d47f314ea5b23b15
Kaloyan Raev <kaloyan.r@zend.com>
2013-10-22 11:50:50 +0300

    ZSTD-3617 Space in Studio install breaks composer dependencies
    On Windows the classpath must be surrounded by double quotes to make
    path with space working correctly.

--- org.eclipse.jgit.pgm.win32/git.bat
+++ org.eclipse.jgit.pgm.win32/git.bat
@@ -1,3 +1,3 @@
 @echo off
 if defined JAVA_HOME (set JAVA="%JAVA_HOME%\bin\java") ELSE (set JAVA=java)
-%JAVA% -cp %JGIT_CLASSPATH% org.eclipse.jgit.pgm.Main %*
+%JAVA% -cp "%JGIT_CLASSPATH%" org.eclipse.jgit.pgm.Main %*
